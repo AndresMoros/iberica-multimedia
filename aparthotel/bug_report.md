e67f27e18b74b25ca270d7f0a16f797d1f265cce
Andrés Moros <andresjmorosa@gmail.com>
2025-06-22 13:31:04 +0200

    Reporte y solución de errores de ApartHotel
    - Dadas las credenciales de ApartHotel y IONOS
    - Creación de bug_report.md para llevar control de los errores del sitio
    - Archivo "apartamentos.php" para encontrar errores en las tablas

---
+++ aparthotel/bug_report.md
@@ -0,0 +1,36 @@
+Enlaces:
+
+# Contenido no corresponde con la URL
+**Estado** [x] 
+https://aparthotelriuebre.com/podas-de-arboles-de-hoja-caduca-a-finales-de-invierno/
+Enlace nuevo: https://aparthotelriuebre.com/comidad-aparthotel-barcelos-rioebro/
+
+# El contenido de algunas tablas está descolocado**
+**Estado** []
+# El final de la pagina tiene dos contenedores sin texto
+**Estado** [x]
+# Cambiar precio de commpra por precio de alquiler
+**Estado** [x]
+https://aparthotelriuebre.com/apartamentos/
+
+# Existe la categoria "podas" en el blog"
+**Estado** [x] 
+https://aparthotelriuebre.com/category/rio_ebro/podas/
+
+# Hay enlaces a redes sociales del creador de el tema usado para la web
+**Estado** [x] 
+https://aparthotelriuebre.com/category/rio_ebro/ 
+
+# El sitemap tiene contenido que no corresponde
+**Estado** [] 
+# Imagen del sitemap no corresponde
+**Estado** [x] 
+https://aparthotelriuebre.com/aviso-legal/sitemap/
+
+# El enlace de política de cookies no encuentra la página de cookies
+**Estado** [] 
+https://aparthotelriuebre.com/aparthotelriuebre.com/aviso-legal/politica-de-cookies/ 
+
+# Imagen principal de Politica de privacidad no correspondiente:
+**Estado** [x] 
+https://aparthotelriuebre.com/aviso-legal/politica-de-privacidad/ 
---
+++ aparthotel/src/apartamentos.php
@@ -0,0 +1,183 @@
+<table style="height: 431px;" width="774">
+    <thead>
+        <tr>
+            <th class="col1">
+                <h3 style="letter-spacing: normal; text-transform: none;"><img class="wp-image-5486 aligncenter" src="../wp-content/uploads/2025/02/Propiedades-barcelona-Rio-Ebro-03-300x225.jpg" alt="Departamento" width="300" height="300" /></h3>
+            </th>
+            <th class="col2" style="text-align: top;">
+                <p style="text-align: left;">Barcelona</p>
+                <p style="text-align: left;">Apartamentos de 1, 2 y 3 dormitorios</p>
+                <p style="text-align: left;">142 viviendas</p>
+                <p style="text-align: left;"><strong>Desde 150 &euro; / D&iacute;a</strong></p>
+            </th>
+            <th class="col3" style="text-align: center;"><a href="#">M&aacute;s informaci&oacute;n</a></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="col1" style="text-align: center;"><strong>TIPO DE INMUEBLE</strong></td>
+            <td class="col2" style="text-align: center;"><strong>DORMITORIOS</strong></td>
+            <td class="col3" style="text-align: center;"><strong>SUPERF&Iacute;CIE</strong></td>
+        </tr>
+        <tr>
+            <td class="col1" style="text-align: center;"><strong>Piso</strong></td>
+            <td class="col2" style="text-align: center;"><strong>2</strong></td>
+            <td class="col3">
+                <p style="text-align: center;"><strong>55m&sup2;</strong></p>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<h3>&nbsp;</h3>
+<p dir="auto">&nbsp;</p>
+
+<table style="height: 276px;" width="757">
+    <thead>
+        <tr>
+            <th class="col1">
+                <h3 style="letter-spacing: normal; text-transform: none;"><img class="alignnone size-medium wp-image-5481" src="../wp-content/uploads/2025/02/Propiedades-barcelona-Rio-Ebro-08-300x169.jpg" alt="" width="300" height="169" /></h3>
+            </th>
+            <th class="col2">
+                <p style="text-align: left;">Barcelona</p>
+                <p style="text-align: left;">Apartamentos de 1, 2 y 3 dormitorios</p>
+                <p style="text-align: left;">142 viviendas</p>
+                <p style="text-align: left;"><strong>Desde 190 &euro; / D&iacute;a</strong></p>
+            </th>
+            <th class="col3"><a href="#">M&aacute;s informaci&oacute;n</a></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="col1"><strong>TIPO DE INMUEBLE</strong></td>
+            <td class="col2"><strong>DORMITORIOS</strong></td>
+            <td class="col3"><strong>SUPERF&Iacute;CIE</strong></td>
+        </tr>
+        <tr>
+            <td class="col1" style="text-align: center;"><strong>Piso</strong></td>
+            <td class="col2" style="text-align: center;">2</td>
+            <td class="col3" style="text-align: center;"><strong>55m&sup2;</strong></td>
+        </tr>
+    </tbody>
+</table>
+<table style="height: 291px;" width="755">
+    <thead>
+        <tr>
+            <th class="col1">
+                <h3 style="letter-spacing: normal; text-transform: none;"><img class="alignnone size-medium wp-image-5481" src="../wp-content/uploads/2025/02/Propiedades-barcelona-Rio-Ebro-08-300x169.jpg" alt="" width="300" height="169" /></h3>
+            </th>
+            <th class="col2" style="text-align: top;">
+                <p style="text-align: left;">Barcelona</p>
+                <p style="text-align: left;">Apartamentos de 1, 2 y 3 dormitorios</p>
+                <p style="text-align: left;">142 viviendas</p>
+                <p style="text-align: left;"><strong>Desde 200 &euro; / D&iacute;a</strong></p>
+            </th>
+            <th class="col3" style="text-align: center;"><a href="#">M&aacute;s informaci&oacute;n</a></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="col1" style="text-align: center;"><strong>TIPO DE INMUEBLE</strong></td>
+            <td class="col2" style="text-align: center;"><strong>DORMITORIOS</strong></td>
+            <td class="col3" style="text-align: center;"><strong>SUPERF&Iacute;CIE</strong></td>
+        </tr>
+        <tr>
+            <td class="col1" style="text-align: center;"><strong>Piso</strong></td>
+            <td class="col2" style="text-align: center;"><strong>2</strong></td>
+            <td class="col3">
+                <p style="text-align: center;"><strong>55m&sup2;</strong></p>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<h3>&nbsp;</h3>
+<p dir="auto">&nbsp;</p>
+
+<table>
+    <thead>
+        <tr>
+            <th class="col1">
+                <h3 style="letter-spacing: normal; text-transform: none;"><img class="alignnone size-medium wp-image-5484" src="../wp-content/uploads/2025/02/Propiedades-barcelona-Rio-Ebro-05-300x225.jpg" alt="" width="300" height="225" /></h3>
+            </th>
+            <th class="col2">
+                <p style="text-align: left;">Barcelona</p>
+                <p style="text-align: left;">Apartamentos de 1, 2 y 3 dormitorios</p>
+                <p style="text-align: left;">142 viviendas</p>
+                <p style="text-align: left;"><strong>Desde 180 &euro; / D&iacute;a</strong></p>
+            </th>
+            <th class="col3"><a href="#">M&aacute;s informaci&oacute;n</a></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="col1"><strong>TIPO DE INMUEBLE</strong></td>
+            <td class="col2"><strong>DORMITORIOS</strong></td>
+            <td class="col3"><strong>SUPERF&Iacute;CIE</strong></td>
+        </tr>
+        <tr>
+            <td class="col1" style="text-align: center;"><strong>Piso</strong></td>
+            <td class="col2" style="text-align: center;">2</td>
+            <td class="col3" style="text-align: center;"><strong>55m&sup2;</strong></td>
+        </tr>
+    </tbody>
+</table>
+<table style="height: 347px;" width="752">
+    <thead>
+        <tr>
+            <th class="col1">
+                <h3 style="letter-spacing: normal; text-transform: none;"><img class="alignnone size-medium wp-image-5485" src="../wp-content/uploads/2025/02/Propiedades-barcelona-Rio-Ebro-04-300x225.jpg" alt="" width="300" height="225" /></h3>
+            </th>
+            <th class="col2" style="text-align: top;">
+                <p style="text-align: left;">Barcelona</p>
+                <p style="text-align: left;">Apartamentos de 1, 2 y 3 dormitorios</p>
+                <p style="text-align: left;">142 viviendas</p>
+                <p style="text-align: left;"><strong>Desde 300 &euro; / D&iacute;a</strong></p>
+            </th>
+            <th class="col3" style="text-align: center;"><a href="#">M&aacute;s informaci&oacute;n</a></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="col1" style="text-align: center;"><strong>TIPO DE INMUEBLE</strong></td>
+            <td class="col2" style="text-align: center;"><strong>DORMITORIOS</strong></td>
+            <td class="col3" style="text-align: center;"><strong>SUPERF&Iacute;CIE</strong></td>
+        </tr>
+        <tr>
+            <td class="col1" style="text-align: center;"><strong>Piso</strong></td>
+            <td class="col2" style="text-align: center;"><strong>2</strong></td>
+            <td class="col3">
+                <p style="text-align: center;"><strong>55m&sup2;</strong></p>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<h3>&nbsp;</h3>
+<p dir="auto">&nbsp;</p>
+
+<table>
+    <thead>
+        <tr>
+            <th class="col1">
+                <h3 style="letter-spacing: normal; text-transform: none;"><img class="wp-image-5486 aligncenter" src="../wp-content/uploads/2025/02/Propiedades-barcelona-Rio-Ebro-03-300x225.jpg" alt="" width="300" height="300" /></h3>
+            </th>
+            <th class="col2">
+                <p style="text-align: left;">Barcelona</p>
+                <p style="text-align: left;">Apartamentos de 1, 2 y 3 dormitorios</p>
+                <p style="text-align: left;">142 viviendas</p>
+                <p style="text-align: left;"><strong>Desde 200 &euro; / D&iacute;a</strong></p>
+            </th>
+            <th class="col3"><a href="#">M&aacute;s informaci&oacute;n</a></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="col1"><strong>TIPO DE INMUEBLE</strong></td>
+            <td class="col2"><strong>DORMITORIOS</strong></td>
+            <td class="col3"><strong>SUPERF&Iacute;CIE</strong></td>
+        </tr>
+        <tr>
+            <td class="col1" style="text-align: center;"><strong>Piso</strong></td>
+            <td class="col2" style="text-align: center;">2</td>
+            <td class="col3" style="text-align: center;"><strong>55m&sup2;</strong></td>
+        </tr>
+    </tbody>
+</table>
